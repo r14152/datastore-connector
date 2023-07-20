fe7082eb44f2fb756ca10db5b63daf92d2a01a3f
Ravi <ravi@lemmatechnologies.com>
2023-07-20 17:38:36 +0530

    adding clickhouse connection

--- clickhouse/clickhouse.go
+++ clickhouse/clickhouse.go
@@ -1,6 +1,7 @@
 package clickhouse
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"time"
@@ -9,8 +10,14 @@ import (
 	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
 )
 
-type ClickhouseConnector struct {
+// ConnectionConnector return driver.Conn
+type ConnectionConnector struct {
 	nativeConn driver.Conn
+}
+
+// DBConnector to return db connection
+type DBConnector struct {
+	DBConn *sql.DB
 }
 
 type ClickhouseConfig struct {
@@ -49,6 +56,34 @@ func NewClickhouseConnector(cfg ClickhouseConfig) (*ClickhouseConnector, error) 
 	}, nil
 }
 
+func NewClickhouseDBConnector(cfg ClickhouseConfig) (*ClickhouseDBConnector, error) {
+	options := &clickhouse.Options{
+		Addr: []string{fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)},
+		Auth: clickhouse.Auth{
+			Database: cfg.DBName,
+			Username: cfg.UserName,
+			Password: cfg.Password,
+		},
+		DialTimeout:  cfg.ConnectTimeout,
+		MaxOpenConns: cfg.MaxOpenConns,
+		MaxIdleConns: cfg.MaxIdleConns,
+	}
+
+	conn := clickhouse.OpenDB(options)
+
+	return &ClickhouseDBConnector{
+		DBConn: conn,
+	}, nil
+}
+
 func (conn *ClickhouseConnector) GetNativeConn() driver.Conn {
 	return conn.nativeConn
+}
+
+// NewMockedClickHouseConnector return mockclient with setting client to given connection
+// its expected to provide mocked conn while calling this api
+func NewMockedClickHouseConnector(conn *sql.DB) *ClickhouseDBConnector {
+	return &ClickhouseDBConnector{
+		DBConn: conn,
+	}
 }
